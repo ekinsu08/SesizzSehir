7e6bbe1141dc3e5262def485b24fcd49418f6e86
ekinsu08 <ekinsu.irem@gmail.com>
2024-01-27 13:11:55 +0300

    login kısmını ekledim logoyu emaili ve şişreyi ekledim

---
+++ lib/view/kaydol_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Kaydol extends StatefulWidget {
+  const Kaydol({super.key});
+
+  @override
+  State<Kaydol> createState() => _KaydolState();
+}
+
+class _KaydolState extends State<Kaydol> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
