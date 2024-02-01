3c3ecdfe6eedf61fdd2590ea113a6dde69924bb3
26ocak2008 <senerelif@gmail.com>
2024-02-01 20:04:20 +0300

    register kısmını yaptım  login here ve register here kısmını yaptım

---
+++ lib/view/RegisterView.dart
@@ -0,0 +1,114 @@
+
+import 'package:flutter/material.dart';
+
+import 'home_view.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+            const SizedBox(
+            height: 100,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+            Icons.account_circle_outlined,
+            size: 40,
+          ),
+          SizedBox(
+            width: 12,
+          ),
+              Text(
+                'Profile App',
+                style: TextStyle(fontSize: 24.5),
+              )
+            ],
+          ),
+
+              const SizedBox(
+                height: 25,
+              ),
+              ClipOval(
+                child: Container(
+                  width: 120.0,
+                  height: 120.0,
+                  child: Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Center(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(fontSize: 24.5),
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: const InputDecoration(hintText: "Name & Surname"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: const InputDecoration(hintText: "Email"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: const InputDecoration(hintText: "Password"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 50,
+                  left: 20,
+                  right: 20,
+                  bottom: 10,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () async {
+
+
+
+
+                      },
+                      child: const Text("Register")),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Anasayfa()));
+                  },
+                  child: const Text("Login Here"))
+
+
+
+          ],
+
+
+          ),
+        ),
+      )
+    );
+
+
+  }
+}
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'RegisterView.dart';
+
 class Anasayfa extends StatelessWidget {
   const Anasayfa({super.key});
 
@@ -11,72 +13,97 @@ class Anasayfa extends StatelessWidget {
         width: double.infinity,
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-
-
-              const Text (
-                'Profile App',
-                style: TextStyle(fontSize: 24.5),
-              ),
-              ClipOval(
-                child: Container(
-                  width: 120.0,
-                  height: 120.0,
-                  child: Image.network(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                    fit: BoxFit.contain,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+            
+            
+                const Text (
+                  'Profile App',
+                  style: TextStyle(fontSize: 24.5),
+                ),
+                ClipOval(
+                  child: Container(
+                    width: 120.0,
+                    height: 120.0,
+                    child: Image.network(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              const Center(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontSize: 24.5),
-                  )),
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration:  InputDecoration(
-                      hintText: "Email",
-                  border: OutlineInputBorder(
-                    borderRadius:
-                      BorderRadius.circular(10.0),
-                  )
-                  ),
-
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration:  InputDecoration(
-                      hintText: "Password",
-                  border: OutlineInputBorder(
-                    borderRadius:
-                      BorderRadius.circular(10.0),
-
-                  )
-                  ),
-
-
-
+                const Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 24.5),
+                    )),
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-
-
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration:  InputDecoration(
+                        hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.circular(10.0),
+                    )
+                    ),
+            
+                  ),
+                ),
+            
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration:  InputDecoration(
+                        hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.circular(10.0),
+            
+                    )
+                    ),
+            
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 50,
+                    left: 20,
+                    right: 20,
+                    bottom: 10,
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          print("Burada");
+            
+                            },
+                        child: const Text("Login")),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterView()));
+                    },
+                    child: const Text("Register Here"))
+            
+            
+              ],
+            ),
           ),
         ),
       ),
