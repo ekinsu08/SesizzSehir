9f60e6004c89a752dfec6daed4f36b41993f1c9d
manizua <manizuaasefi@gmail.com>
2024-02-08 19:55:55 +0300

    hocamız yazdı biz hiç bir şey yapmadıkk :(

---
+++ lib/lib/services/animalModel.dart
@@ -0,0 +1,14 @@
+
+class AnimalModel{
+
+
+
+  Map<String,String> categoryList = {
+    "All": "https://media.istockphoto.com/id/1296353202/photo/group-of-pets-posing-around-a-border-collie-dog-cat-ferret-rabbit-bird-fish-rodent.jpg?s=612x612&w=0&k=20&c=l4UJze9hXcNABS_3QJcaOU07R1pcuV3L7w_IJTu9o2c=",
+    "Cat" : "https://as1.ftcdn.net/v2/jpg/02/95/94/94/1000_F_295949484_8BrlWkTrPXTYzgMn3UebDl1O13PcVNMU.jpg",
+    "Dog" :"https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg",
+    "Bird" :"https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
+    "Fish" :"https://images.pexels.com/photos/128756/pexels-photo-128756.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "Rabbit":"https://assets.petco.com/petco/image/upload/f_auto,q_auto/rabbit-care-sheet"
+  };
+}
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sesizz_sehir/lib/services/animalModel.dart';
 import 'package:sesizz_sehir/view/RegisterView.dart';
 
 class HomeView extends StatefulWidget {
@@ -31,7 +32,7 @@ class _HomeViewState extends State<HomeView> {
               SizedBox(
                 height: 65,
                 child: ListView.builder(
-                  itemCount: 5,
+                  itemCount: AnimalModel().categoryList.length,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context , index){
                       return GestureDetector(
@@ -43,8 +44,11 @@ class _HomeViewState extends State<HomeView> {
                         child: Card(
 
                           color: index == selcted ? Colors.pink.shade100 : Colors.white,
+                          elevation: index == selcted ? 3:0,
                           shape: RoundedRectangleBorder(
+                            side: index != selcted ? BorderSide(color: Colors.grey, width: 1) : BorderSide.none,
                             borderRadius: BorderRadius.circular(50),
+
                           ),
                           child:Padding(
                             padding:EdgeInsets.symmetric(vertical: 8,horizontal: 16),
@@ -57,15 +61,15 @@ class _HomeViewState extends State<HomeView> {
                                     width: 40.0,
                                     height: 40.0,
                                     child: Image.network(
-                                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                                      fit: BoxFit.contain,
+                                      AnimalModel().categoryList[AnimalModel().categoryList.keys.elementAt(index)]!,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
                                 ),
 
                                 Padding(
                                   padding: const EdgeInsets.only(left: 15.0,right: 8),
-                                  child: Text('ALL', style: TextStyle(fontSize: 20.5, fontWeight: FontWeight.w600,),),
+                                  child: Text(AnimalModel().categoryList.keys.elementAt(index), style: TextStyle(fontSize: 20.5, fontWeight: FontWeight.w600,),),
 
 
                                 ),
@@ -79,18 +83,92 @@ class _HomeViewState extends State<HomeView> {
                 ),
 
               ),
-              Card(
-                child: Column(
+              SizedBox(
+                height: 24,
+              ),
+
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 41,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Card(
+
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 100,
+
+                              child: Stack(
+                                fit: StackFit.expand,
+                                children: [
+                                  Image.network(
+                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                    fit: BoxFit.cover,
+                                  ),
+
+                                  Positioned(
+                                      right: 0,
+                                      child: Icon(Icons.star_border_outlined,size: 33,color: Colors.white,)),
 
 
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+
+                                    children: [
+                                      Text('GARY', style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w600,),),
+                                      Spacer(),
+                                      Text('3 years old', style: TextStyle(fontSize: 15.5,),),
+
+                                    ],
+
+                                  ),
+                                  Text('Bird', style: TextStyle(fontSize: 18.5, ),),
+
+                                ],
+
+                              ),
+                            ),
+
+                          ],
+                        ),
+
+
+
+
+
+                      ),
+                    );
+                  },
+                
+                
+                
                 ),
               ),
+
             ],
 
           ),
 
 
 
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
         ),
       ),
     );
